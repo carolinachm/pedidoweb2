2b8d973257371baedda429103bd0a800a87f153f
Carolina <carolti2013@gmail.com>
2016-09-30 10:14:55 -0300

    Criação da classe LoginController

---
+++ src/main/java/br/com/fdp/pedidos/controller/LoginController.java
@@ -0,0 +1,32 @@
+package br.com.fdp.pedidos.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import lombok.Getter;
+import lombok.Setter;
+import br.com.fdp.pedidos.model.User;
+
+@Named
+@ViewScoped
+public class LoginController {
+	@Getter@Setter
+	private User user = new User();
+	
+	 public String doEfetuarLogin() {
+		    if(user.equals(user.getLogin()) &&
+		       user.equals(user.getSenha())) {
+		      /* Se escrever o login e senha correto então vai para a tela principal do sistema. */
+		      return "index";
+		    } else {
+		        /* Cria uma mensagem. */
+		        FacesMessage msg = new FacesMessage("Usuário ou senha inválido!");
+		        /* Obtém a instancia atual do FacesContext e adiciona a mensagem de erro nele. */
+		        FacesContext.getCurrentInstance().addMessage("erro", msg);
+		        return null;
+		    }
+		  }
+
+}
