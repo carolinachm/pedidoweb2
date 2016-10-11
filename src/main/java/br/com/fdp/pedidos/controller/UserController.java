f68a630d3d3b951ce37a7e6e578f217611ff94a9
carolina <carolti2013@gmail.com>
2016-10-11 19:18:03 -0300

    atualização da userController

--- src/main/java/br/com/fdp/pedidos/controller/UserController.java
+++ src/main/java/br/com/fdp/pedidos/controller/UserController.java
@@ -6,31 +6,31 @@ import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 
-
-
-
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.fdp.pedidos.model.Product;
 import br.com.fdp.pedidos.model.User;
 import br.com.fdp.pedidos.repository.UserRepository;
+import br.com.fdp.pedidos.util.MensagemUtil;
 
 @Named
 @ViewScoped
-
 public class UserController {
-	@Getter@Setter
+	@Getter
+	@Setter
 	private User user = new User();
-	@Getter@Setter
+	@Getter
+	@Setter
 	private List<User> users;
 	@Autowired
 	private UserRepository userRepository;
-	@Getter@Setter
+	@Getter
+	@Setter
 	private boolean modoEdicao = false;
 
 	@PostConstruct
@@ -39,38 +39,46 @@ public class UserController {
 	}
 
 	public void save() {
+		try {
+			userRepository.save(getUser());
+			if (!isModoEdicao())
+				users.add(user);
+			user = new User();
+			setModoEdicao(false);
+			MensagemUtil.mensagemAviso("Salvo com sucesso");
+		} catch (ServiceException e) {
+			e.printStackTrace();
+			MensagemUtil.mensagemErro("Erro ao Salvar." + e.getMessage());
+		}
 
-		userRepository.save(getUser());
-		if (!isModoEdicao())
-			users.add(user);
-		user = new User();
-		setModoEdicao(false);
-
-	}
-
-	public void limpar() {
-		user = new User();
-	}
-
-	public void listar() {
-		userRepository.findAll();
 	}
 
 	public void remover(User user) {
-		userRepository.delete(user);
-		users.remove(user);
-		user = new User();
+		try {
+			userRepository.delete(user);
+			users.remove(user);
+			user = new User();
+			MensagemUtil.mensagemAviso("Excluido com sucesso");
+		} catch (ServiceException e) {
+			e.printStackTrace();
+			MensagemUtil.mensagemErro("Erro ao excluir." + e.getMessage());
+		}
 	}
 
 	public void editar(User user) {
+		try{
 		setUser(user);
 		setModoEdicao(true);
+		MensagemUtil.mensagemAviso("Alterado com sucesso");
+		} catch (ServiceException e) {
+			e.printStackTrace();
+			MensagemUtil.mensagemErro("Erro ao alterar." + e.getMessage());
+		}
 	}
 
 	public void cancelar() {
 		user = new User();
 		setModoEdicao(false);
 	}
-
 
 }
